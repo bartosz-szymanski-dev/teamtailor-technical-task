165c9b1834ce02cb89fe05e981e1e7e5bd709efb
Bartosz Szymański <szymaskibartosz@gmail.com>
2024-10-29 21:58:38 +0100

    [e2e-tests-fail] Fixed the E2E tests and adjusted return type for single iteration in map function

--- src/csv/processors/csv.processor.ts
+++ src/csv/processors/csv.processor.ts
@@ -8,27 +8,29 @@ export default class CsvProcessor {
   public process(
     candidateResponse: CandidateResponseInterface,
   ): ProcessorOutputInterface[] {
-    return candidateResponse.data.map((candidate: CandidateInterface) => {
-      const {
-        email,
-        'first-name': firstName,
-        'last-name': lastName,
-      } = candidate.attributes;
-      const jobApplication =
-        candidate.relationships['job-applications'].data[0];
-      const jobAppData = candidateResponse?.included.find(
-        (app) => app.id === jobApplication.id,
-      );
-      const jobApplicationCreatedAt = jobAppData?.attributes['created-at'];
+    return candidateResponse.data.map(
+      (candidate: CandidateInterface): ProcessorOutputInterface => {
+        const {
+          email,
+          'first-name': firstName,
+          'last-name': lastName,
+        } = candidate.attributes;
+        const jobApplication =
+          candidate.relationships['job-applications']?.data[0];
+        const jobAppData = candidateResponse?.included.find(
+          (app) => app.id === jobApplication.id,
+        );
+        const jobApplicationCreatedAt = jobAppData?.attributes['created-at'];
 
-      return {
-        candidate_id: candidate.id,
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        job_application_id: jobApplication?.id,
-        job_application_created_at: jobApplicationCreatedAt,
-      };
-    });
+        return {
+          candidate_id: candidate.id,
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          job_application_id: jobApplication?.id,
+          job_application_created_at: jobApplicationCreatedAt,
+        };
+      },
+    );
   }
 }
