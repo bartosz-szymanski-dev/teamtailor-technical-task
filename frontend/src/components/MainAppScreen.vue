e5e2c4ea314f85f1194f5e2d934c058982adab46
Bartosz Szymański <szymaskibartosz@gmail.com>
2024-11-05 16:07:18 +0100

    [missing-functionallity] Introduced structure

---
+++ frontend/src/components/MainAppScreen.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-center fill-height mx-auto"
+      max-width="900"
+    >
+      <div class="text-center">
+        <div class="text-body-2 font-weight-light mb-n1">Technical Task</div>
+
+        <h1 class="text-h2 font-weight-bold">Teamtailor</h1>
+
+        <strong class="text-body-2 font-weight-light mt-1">by Bartosz Szymański</strong>
+      </div>
+
+      <div class="py-4" />
+
+      <v-row>
+        <v-col cols="12">
+          <v-btn
+            block
+            :disabled="appStore.loading"
+            :loading="appStore.loading"
+            @click="appStore.initDownload()"
+          >Download candidates data</v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  import { useAppStore } from '@/stores/app'
+
+  const appStore = useAppStore()
+</script>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <MainAppScreen />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
